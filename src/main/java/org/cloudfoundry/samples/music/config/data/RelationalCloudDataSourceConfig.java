34e2b6fcc83ad83bf7931a0987abcdf8f0811ccb
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2015-04-09 15:25:33 +0200

    Set poolsize to 40

--- src/main/java/org/cloudfoundry/samples/music/config/data/RelationalCloudDataSourceConfig.java
+++ src/main/java/org/cloudfoundry/samples/music/config/data/RelationalCloudDataSourceConfig.java
@@ -1,6 +1,10 @@
 package org.cloudfoundry.samples.music.config.data;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,12 +12,23 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 
 @Configuration
-@Profile({"mysql-cloud", "postgres-cloud", "oracle-cloud"})
+@Profile({"mysql-cloud", "postgres-cloud", "oracle-cloud", "cloud"})
 public class RelationalCloudDataSourceConfig extends AbstractCloudConfig {
+    private static final Log logger = LogFactory.getLog(RelationalCloudDataSourceConfig.class);
 
     @Bean
     public DataSource dataSource() {
-        return connectionFactory().dataSource();
+        PooledServiceConnectorConfig.PoolConfig poolConfig = new PooledServiceConnectorConfig.PoolConfig(40, 30000);
+        DataSourceConfig.ConnectionConfig connectionConfig =
+                new DataSourceConfig.ConnectionConfig("characterEncoding=UTF-8");
+        DataSourceConfig serviceConfig =
+                new DataSourceConfig(poolConfig, connectionConfig);
+
+        DataSource source = connectionFactory().dataSource(serviceConfig);
+
+        logger.info("Returning configured DataSource object");
+
+        return source;
     }
 
 }
